5abae44fa8e66ec7ecde516085d57f9e96a788a2
Keshav-Lenovo <keshavbhatt@outlook.com>
2021-09-01 18:48:36 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # NLSummerWeather
+Ik was benieuwd over deze tweet https://twitter.com/whoebert/status/1432757193871400972
+want ik dacht ook dat de zomer van 2021 koud was 
+
+- Data is van https://www.knmi.nl/nederland-nu/klimatologie/daggegevens 
+  locatie _De Bilt_
+ 
+
+Het is opvallend dat het langzaam wordt warmer vergeleken met 70 jaar historische data. 
+De effecten van _global warming_ zijn in dat manier zichtbaar. Het is warmer eigenlijk ongeacht van welke temperatuur variable je kijkt _i.e._ alle indicatoren: mean, min and max temperaturen zijn hoger in de recent 1/3/30 jaren vergeleken met data van 1951 t/m nu. Wat is ook opvallend is dat vroeger was er meer wind in Nederland. 
+
+
+Alhoewel de belangrijkste is de beleving want weer is absoluut maar ook relatief. De probleem met dit tweet is dat het numeric is en de effecten wij voelen, onze klagen zijn relatief. Oftewel je kan niet maar op een variabel kijken bijv. temperatuur of wind maar de combinatie is belangrijk. Daarom heb ik een visualizatie met meerdere variablen gemaakt
+
+De effecten van relatief meer regen, meer wolken en minder zon speelt een groter rol in de beleving dat deze was een koude zomer.
+
+
+Maar wat blijkt ook is dat Nederland gewoon een koude land is maar de afgelopen drie jaaren hebben we hebben een verwachting gemaakt van een warme tropische zomer
+
+Volgens mij dit is een Availability Heuristic Cognitive Bias 
+
+**Availability Heuristic**
+
+- A mental shortcut that relies on immediate examples that come to a given person's mind when evaluating a specific topic, concept, method or decision.
+- People tend to heavily weigh their judgments toward more recent information, making new opinions biased toward the latest news.
--- analysis.R
+++ analysis.R
@@ -1,22 +1,22 @@
 library(tidyverse)
 library(lubridate)
-setwd('C:/Users/M64H098/repo/zomer')
+setwd('C:/NLSummerWeather')
 bilt <- read.csv('data_weer.txt')
 
-bilt <- bilt %>% rename(vec_mean_windsp = FHVEC, mean_windsp = FG, mean_tem = TG, 
-                        min_tem = TN, max_tem = TX, 
+# Rename to more meaningful col names
+bilt <- bilt %>% rename(vec_mean_windsp = FHVEC, mean_windsp = FG, mean_temp = TG, 
+                        min_temp = TN, max_temp = TX, 
                         sunshine_duration = SQ, precipitation_hr = DR, 
                         precipitation_mm = RH, cloud_cover = NG)
 
-# Selected because NG is not NA here
+# Select relevant cols and filter after 1951 because cloud cover variable is previously unfilled
 bilt_70 <-
   bilt %>% filter(YYYYMMDD > 19510101) %>% select(
     YYYYMMDD,
-    vec_mean_windsp,
     mean_windsp,
-    mean_tem,
-    min_tem,
-    max_tem,
+    mean_temp,
+    min_temp,
+    max_temp,
     sunshine_duration,
     precipitation_hr,
     precipitation_mm,
@@ -24,55 +24,56 @@ bilt_70 <-
   )
 
 # Daily mean temperature in (0.1 degrees Celsius)
-bilt_70$mean_tem <- bilt_70$mean_tem/10
-bilt_70$min_tem <- bilt_70$mean_tem/10
-bilt_70$max_tem <- bilt_70$mean_tem/10
+bilt_70$mean_temp <- bilt_70$mean_temp/10
+bilt_70$min_temp <- bilt_70$mean_temp/10
+bilt_70$max_temp <- bilt_70$mean_temp/10
 
 # Convert column to date
 bilt_70$YYYYMMDD <- lubridate::ymd(bilt_70$YYYYMMDD)
+
 bilt_70 <- na.omit(bilt_70)
 
-# Select summer months only
+# Select summer months (July, Aug) only 
 bilt_70_summer <- bilt_70 %>% filter(month(YYYYMMDD) %in% c(7,8))
-bilt_70_summer_num <- bilt_70 %>% select(-c(YYYYMMDD, vec_mean_windsp))
+# Select only numeric cols
+bilt_70_summer_num <- bilt_70 %>% select(-YYYYMMDD)
 
 # Filter last 30 years
 bilt_30_summer <- bilt_70_summer %>% filter(year(YYYYMMDD) > 1990)
-bilt_30_summer_num <- bilt_30_summer %>% select(-c(YYYYMMDD, vec_mean_windsp))
+bilt_30_summer_num <- bilt_30_summer %>% select(-YYYYMMDD)
 
 # Filter last 3 years
 bilt_3_summer <- bilt_70_summer %>% filter(year(YYYYMMDD) %in% c(2019, 2020, 2021))
-bilt_3_summer_num <- bilt_3_summer %>% select(-c(YYYYMMDD, vec_mean_windsp))
+bilt_3_summer_num <- bilt_3_summer %>% select(-YYYYMMDD)
 
-# Filter last 1 year
+# Filter this year
 bilt_1_summer <- bilt_70_summer %>% filter(year(YYYYMMDD) %in% 2021)
-bilt_1_summer_num <- bilt_3_summer %>% select(-c(YYYYMMDD, vec_mean_windsp))
+bilt_1_summer_num <- bilt_3_summer %>% select(-YYYYMMDD)
 
-# Just means
-colMeans(bilt_1_summer_num)
-colMeans(bilt_3_summer_num)
-colMeans(bilt_30_summer_num)
+# Just find means for demonstration
 colMeans(bilt_70_summer_num)
+colMeans(bilt_30_summer_num)
+colMeans(bilt_3_summer_num)
+colMeans(bilt_1_summer_num)
 
+### Plotting ###
+# Convert means into a df
 mean_70 <- data.frame(mean = colMeans(bilt_70_summer_num), variable = names(bilt_70_summer_num), history = '70j')
 mean_30 <- data.frame(mean = colMeans(bilt_30_summer_num), variable = names(bilt_30_summer_num), history = '30j')
 mean_3 <- data.frame(mean = colMeans(bilt_3_summer_num), variable = names(bilt_3_summer_num), history = '3j')
 mean_1 <- data.frame(mean = colMeans(bilt_1_summer_num), variable = names(bilt_1_summer_num), history = '1j')
 
+# Bind these means
 bound_means <- rbind(mean_70, mean_30, mean_3, mean_1)
 
 bound_means$history <- as.factor(bound_means$history)
 levels(bound_means$history) <- c('1j', '3j', '30j' ,'70j')
+bound_means$variable <- as.factor(bound_means$variable)
+levels(bound_means$variable) <- c('min_temp', 'mean_temp', 'max_temp', 'mean_windsp', 'cloud_cover',
+                                  'sunshine_duration', 'precipitation_hr', 'precipitation_mm')
 
-
-# grouped
+# Plot dodged boxplots
 ggplot(bound_means, aes(fill = history, y = mean, x = variable)) + 
   geom_bar(position="dodge", stat="identity") 
   
 
-op basis van andere tweets (
-  @pr8npraal
-  
-  
-  @emiloost
-) ik heb de data van De Bilt analyzed en het blijkt dat we cognitive dissonance hebben. Nederlandse zomer zijn idd zoals van deze zomer
